691bf4bca9858d827dec8f8d64cc5a7b6fb140d0
umpirsky <umpirsky@gmail.com>
2015-01-17 21:43:57 +0100

    Fix tag section invalid in Entity

--- src/Centipede/Extractor/UrlExtractor.php
+++ src/Centipede/Extractor/UrlExtractor.php
@@ -9,7 +9,11 @@ class UrlExtractor implements ExtractorInterface
         $urls = [];
 
         $document = new \DOMDocument();
+
+        libxml_use_internal_errors(true);
         $document->loadHTML($value);
+        libxml_use_internal_errors(false);
+
         foreach ($document->getElementsByTagName('a') as $node) {
             $urls[] = $node->getAttribute('href');
         }
